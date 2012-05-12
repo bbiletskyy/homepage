4fe6d20484b7d18580c623ce020f11b25e734abd
Borys Biletskyy <borys.biletskyy@gmail.com>
2012-05-12 18:19:10 +0300

    Impressing Item model improved

--- src/main/scala/homepage/model/ImpressingItem.scala
+++ src/main/scala/homepage/model/ImpressingItem.scala
@@ -4,7 +4,7 @@ import net.liftweb.mapper._
 
 object ImpressingItem extends ImpressingItem with KeyedMetaMapper[Long, ImpressingItem] {
   override def dbTableName = "impressing_item"
-  override def fieldOrder = id :: title :: content :: Nil
+  override def fieldOrder = id :: title :: content :: youtubeIds :: Nil
 }
 
 class ImpressingItem extends KeyedMapper[Long, ImpressingItem] {
@@ -14,6 +14,8 @@ class ImpressingItem extends KeyedMapper[Long, ImpressingItem] {
   object id extends MappedLongIndex(this)
   object title extends MappedString(this, 1024)
   object content extends MappedText(this)
+  object youtubeIds extends MappedString(this, 64)
+  object tag extends MappedString(this, 32)
   //  object content extends MappedTextarea(this, 2048) {
   //    override def textareaRows  = 10
   //    override def textareaCols = 50
--- src/main/scala/homepage/snippet/PostImpressingItem.scala
+++ src/main/scala/homepage/snippet/PostImpressingItem.scala
@@ -12,41 +12,30 @@ import scala.xml.Node
 class PostImpressingItem {
   object titleReqVar extends RequestVar(Full(""))
   object contentReqVar extends RequestVar(Full(""))
+  object youtubeIdsReqVar extends RequestVar(Full(""))
+  object tagReqVar extends RequestVar(Full(""))
 
+  
   def add(xhtml: NodeSeq): NodeSeq = {
-    val titleOk = !(titleReqVar.isEmpty || titleReqVar.open_!.length == 0)
-    val contentOk = !(contentReqVar.isEmpty || contentReqVar.open_!.length == 0)
-    if (titleOk && contentOk) {
+    if (validateReqVars) {
       val ii: ImpressingItem = ImpressingItem.create
-      ii.title(titleReqVar.open_!).content(contentReqVar.open_!).save
+      ii.title(titleReqVar.open_!).content(contentReqVar.open_!).youtubeIds(youtubeIdsReqVar.open_!).tag(youtubeIdsReqVar.open_!).save
     }
     bind("ii", xhtml,
-      "title" --> text("", v => titleReqVar(Full(v))) % ("size" -> "50") % ("id" -> "title"),
+      "title" --> text("Title", v => titleReqVar(Full(v))) % ("size" -> "50") % ("id" -> "title"),
       "content" --> text("", v => contentReqVar(Full(v))) % ("size" -> "50") % ("id" -> "content"),
+      "youtubeIds" --> text("", v => youtubeIdsReqVar(Full(v))) % ("size" -> "50") % ("id" -> "youtubeIds"),
+      "tag" --> text("", v => tagReqVar(Full(v))) % ("size" -> "32") % ("id" -> "tag"),
       "submit" --> submit(?("Add Impressing Item"), () => {}))
   }
 
-  def impressingItems(xhtml: NodeSeq): NodeSeq = {
-    //val xxx = ImpressingItem.findAll
-    //val nn = NodeSeq.fromSeq(Seq(<p>123</p>, <p>321</p>))
-
-    //val res = ImpressingItem.findAll
-
-    val xml = ImpressingItem.findAll.map(i => <p>{ i.title } -- { i.content }</p>).toSeq
-    xml
+  private def validateReqVars: Boolean = {
+    val titleOk = !(titleReqVar.isEmpty || titleReqVar.open_!.length == 0)
+    val contentOk = !(contentReqVar.isEmpty || contentReqVar.open_!.length == 0)
+    val youtubeIdsOk = !(youtubeIdsReqVar.isEmpty || youtubeIdsReqVar.open_!.length == 0)
+    val tagOk = !(tagReqVar.isEmpty || tagReqVar.open_!.length == 0)
+    titleOk && contentOk && youtubeIdsOk && tagOk
   }
 
-  //  def show(xhtml: NodeSeq): NodeSeq = {
-  //    val msgSent = !(qpx.isEmpty || qpx.open_!.length == 0)
-  //    if (msgSent) {
-  //      val msg: ImpressingItem = ImpressingItem.create
-  //      msg.title(qpx.open_!).save
-  //    }
-  //    val impressingItems = ImpressingItem.findAll
-  //    val temp = impressingItems.foldLeft("") { (str, item) => str + " ### " + item.title }
-  //    bind("qp", xhtml,
-  //      "update" --> text("", v => qpx(Full(v))) % ("size" -> "50") % ("id" -> "update"),
-  //      "submit" --> submit(?("Update"), () => {}),
-  //      "messages" --> <div>{ temp }</div>)
-  //  }
+  def impressingItems(xhtml: NodeSeq): NodeSeq = ImpressingItem.findAll.map(i => <p>{ i.title } <br/> { i.content } - { i.youtubeIds } - { i.tag } </p>).toSeq
 }
