90ad7b5b695d7e75056f55c0fdbb551d82f98486
Borys Biletskyy <borys.biletskyy@gmail.com>
2012-05-24 22:07:35 +0300

    About image added

--- src/main/scala/homepage/snippet/Image.scala
+++ src/main/scala/homepage/snippet/Image.scala
@@ -6,6 +6,7 @@ class Image {
     case s if s.contains("publications") => <img src="/images/publications.gif"/>
     case s if s.contains("research") => <img src="/images/research.gif"/>
     case s if s.contains("contact") => <img src="/images/contacts.gif"/>
+      case s if s.contains("about") => <img src="/images/about.gif"/>
     case _ => <img src="/images/impressing.gif"/>
   }
 }
